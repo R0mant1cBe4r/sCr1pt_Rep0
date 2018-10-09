caea027660cc809242ef2b17118808800770fbd3
Ljx-Daddy <43593369+Ljx-Daddy@users.noreply.github.com>
2018-10-09 11:31:36 +0800

    Add files via upload

---
+++ sha-512.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+import string
+import hashlib
+import datetime
+
+#文件读取
+def pwdread(path):
+    pwd = []
+    f = open(path, 'r')
+    line = f.readline()
+    while line :
+        l = str(line).strip()
+        pwd.append(l)
+        line = f.readline()
+    f.close()
+    return pwd
+
+#文件写入
+def urlwrite(path_, pwd):
+    f_ = open(path_, 'a+')  #执行文件之前先将旧文件删除
+    f_.write(pwd+"\n")
+    f_.flush()
+    f_.close()
+
+#defsha512
+def sha_512(pwd):
+    salt = "hV2VRhZO"
+    pwd_hash = hashlib.sha512()
+    pwd_hash.update(salt+pwd)
+    return pwd_hash.hexdigest()
+
+def main(path, path_):
+    passwd = []
+    passwd = pwdread(path)
+    for pwd in passwd:
+        urlwrite(path_, sha_512(pwd))   
+
+if __name__ == '__main__':
+    e1 = datetime.datetime.now()
+    main("oracle_pwd.txt", "oracle_hash512_pwd.txt")
+    e2 = datetime.datetime.now()
+    print("Spend time:"+str(e2-e1))
+
+
